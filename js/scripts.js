bda78b9cccba26772cdbd95c61b44f7935e53199
jorgesoares2997 <jorgesoares2997@gmail.com>
2023-04-06 11:35:30 -0300

    scroll

--- js/scripts.js
+++ js/scripts.js
@@ -126,4 +126,45 @@ $(document).ready(function() {
             });
         }
     }
+    // Scroll para seções 
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo ='';
+    $(navBtn).click(function(){
+        let btnId = $(this).attr('id');
+
+        if(btnId == 'about-menu'){
+            scrollTo=aboutSection;
+        }else if (btnId == 'team-menu'){
+            scrollTo = teamSection;
+
+        }else if (btnId == 'team-menu'){
+            scrollTo = teamSection;
+
+        }else if (btnId == 'portfolio-menu'){
+            scrollTo = portfolioSection;
+
+        }else if (btnId == 'contact-menu'){
+            scrollTo = contactSection;
+
+        }else if (btnId == 'services-menu'){
+            scrollTo = servicesSection;
+        }else {
+            scrollTo=bannerSection
+        }
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+    }, 1500);
+
+        
+    });
+   
+
 });
